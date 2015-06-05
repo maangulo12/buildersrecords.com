c3f88c5b436a2b26687207a1809c90fa893f8b71
Miguel Angulo <maangulo12@gmail.com>
2015-06-04 23:20:03 -0500

    Add CSRF protection

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,26 @@
 """
 
 
-from flask import Flask
+from flask import *
+from flask_wtf.csrf import CsrfProtect
 
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
+csrf = CsrfProtect()
+csrf.init_app(app)
+
+
+@csrf.error_handler
+def csrf_error(reason):
+    """
+    Route for url: csrf error
+    """
+    return render_template('csrf_error.html', reason=reason)
+
+
+from app import db
+from app import forms
+from app import mail
 from app import views
